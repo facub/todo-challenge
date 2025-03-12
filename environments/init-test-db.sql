8aaedba7823c11f15800ed2afd850a15a4639d4a
Facundo Buc <54050346+facub@users.noreply.github.com>
2025-03-11 22:18:21 -0300

    init.sql modularization

--- environments/init-test-db.sql
+++
@@ -1,4 +0,0 @@
--- Create the user with permissions to create databases
-CREATE USER test_user WITH PASSWORD 'test_password' CREATEDB;
--- Create the database
-CREATE DATABASE test_todo_db OWNER test_user;
